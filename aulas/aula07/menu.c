c3413fba3d32739280214af73c4bc3adf9f566b2
pantufas <161594829+marceeell@users.noreply.github.com>
2024-04-29 13:46:56 +0000

    codigos da aula 7 e pratica 3

---
+++ aulas/aula07/menu.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+  int opcao;
+
+  printf("MENU PRINCIPAL\n");
+  printf("1 - Consultar saldo\n");
+  printf("2 - Recarregar saldo\n");
+  printf("3 - Ligações recebidas\n");
+  printf("4 - Ligações feitas\n");
+  printf("0 - Sair\n");
+  printf("Entre com uma opcao: ");
+  int leu_certo = scanf("%i", &opcao);
+
+  switch (opcao) {
+  case 1:
+    printf("Seu saldo é de R$ 10.00\n");
+    break;
+  case 2: {
+    float valor;
+    printf("Entre com o valor da recarga:\n");
+    leu_certo = scanf("%f", &valor);
+    break;
+  }
+  case 3: {
+    printf("As 5 ultimas ligações recebidas\n");
+    printf("9999-9999\n");
+    printf("8888-8888\n");
+    printf("6666-6666\n");
+    break;
+  }
+  case 4: {
+    printf("As 5 ultimas ligações recebidas\n");
+    printf("9999-9999\n");
+    printf("8888-8888\n");
+    printf("6666-6666\n");
+    break;
+  }
+  case 0:
+    printf("BYE BYE🤫🧏‍♂️!\n");
+    break;
+
+  default:
+    printf("Opção invalida. Tente novamente!\n");
+  }
+  return 0;
+}
