c6720d80434d3001aa1c5edd48502e0088e176e2
pantufas <161594829+marceeell@users.noreply.github.com>
2024-05-13 13:49:56 +0000

    codigos aula 8 e pratica 3

---
+++ aulas/aula08/validacao.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+  int numero;
+  int numero_eh_valido = 0;
+
+  while (numero_eh_valido == 0) {
+    printf("Entre com um numero entre 1 e 10: ");
+    int leu_certo = scanf("%i", &numero);
+
+    numero_eh_valido = leu_certo && numero >= 1 && numero <= 10;
+
+    if (numero_eh_valido == 0) {
+      printf("Algo de errado naum ta certo!\n");
+      getchar(); // limpar o buffer
+    }
+  }
+
+  printf("Faz certo que da certo!\n");
+
+  return 0;
+}
